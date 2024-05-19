9cd07e44afba4f96a8ff4df1fbbd1b60ae06e0b5
MiguelCGa <micurros@ucm.es>
2024-05-19 23:10:33 +0200

    fanfarria

--- Final/Assets/Scripts/Characters/CharactersManager.cs
+++ Final/Assets/Scripts/Characters/CharactersManager.cs
@@ -2,13 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using FMOD.Studio;
+using FMODUnity;
 
 public class CharactersManager : MonoBehaviour
 {
+    [SerializeField] EventReference victorySound;
+    [SerializeField] float timeFromVictoryToMelody = 1.5f;
+    float timeElapsedFromVictory = 0.0f;
     [SerializeField] PianoManager piano;
     [SerializeField] GameObject[] characters;
     List<GameObject> strangers = new List<GameObject>();
     List<GameObject> friends = new List<GameObject>();
+    bool victoriing = false;
 
     // Start is called before the first frame update
     void Awake()
@@ -18,6 +24,16 @@ public class CharactersManager : MonoBehaviour
         }
         strangers.First().SetActive(true);
         PlayCurrentMelody();
+    }
+
+    void Update() {
+        if (victoriing) {
+            timeElapsedFromVictory += Time.deltaTime;
+            if (VictorySoundFinished()) {
+                victoriing = false;
+                strangers.First().GetComponent<Sequence>().PlayMelody();
+            }
+        }
     }
 
     public Sequence BefriendCurrentCharacter() {
@@ -35,9 +51,8 @@ public class CharactersManager : MonoBehaviour
         }
         
         strangers.First().SetActive(true);
-        Sequence nextSequence = strangers.First().GetComponent<Sequence>();
-        nextSequence.PlayMelody();
-        return nextSequence;
+        PlayVictorySound();
+        return strangers.First().GetComponent<Sequence>();
     }
 
     public void PlayCurrentMelody() {
@@ -46,10 +61,19 @@ public class CharactersManager : MonoBehaviour
         }
     }
 
-    public Sequence getCurrentCharacter()
-    {
-        if(strangers.Count >0)
-        return strangers.First().GetComponent<Sequence>();
+    public Sequence getCurrentCharacter() {
+        if (strangers.Count > 0)
+            return strangers.First().GetComponent<Sequence>();
         return null;
+    }
+
+    private void PlayVictorySound() {
+        RuntimeManager.PlayOneShot(victorySound);
+        victoriing = true;
+        timeElapsedFromVictory = 0.0f;
+    }
+
+    private bool VictorySoundFinished() {
+        return timeFromVictoryToMelody <= timeElapsedFromVictory;
     }
 }
